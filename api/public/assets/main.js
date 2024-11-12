1469b56e52317c304aa3eefaa9961258f6ac57da
Sebastian-105 <sebastianschapfel105@gmail.com>
2024-11-12 19:14:45 +0000

    asdf

--- api/public/assets/main.js
+++
@@ -1,8 +0,0 @@
-function getUserData(type) {
-  let user_id = document.getElementById('userID').value;
-  if (type == 'simple') {
-    window.open(`/v2/account/getSimpleProfile?id=${user_id}`);
-  } else if (type == 'full') {
-    window.open(`/v2/account/getProfile?id=${user_id}`)
-  }
-}
---
+++ assets/main.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector(".exit");
+ 
+let exitBtn = document.querySelector(".exit");
+exitBtn.addEventListener("click", (e) => {
+  menu.style.display = "none";
+
+})
+
