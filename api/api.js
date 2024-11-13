0d880234525dcb74066b1fa28f5237fdfce30d83
Sebastian-105 <sebastianschapfel105@gmail.com>
2024-11-13 19:34:25 +0000

    a

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
+const port = 8000;
 const path = require('path');
 const fs = require('fs');
 
@@ -242,10 +242,10 @@ async function api() {
       });
     }
   });
-  app.get('/v2/getAllTop50', async (req, res) => {
+  app.get('/v2/getAllTop50', async (req, res) => { //Search user
     const allSSN = [
-      11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-      30, 31, 32, 33, 34, 35, 36,
+      10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+      30, 31, 32, 33, 34, 35, 36
     ];
     const { name, userID } = req.query;
 
---
+++ api/public/assets/main.js
@@ -0,0 +1,30 @@
+let titleContent = document.querySelector("#title");
+let loader = document.querySelector(".loader");
+let loaderBackground = document.querySelector(".loaderBackground");
+let returnedContent = document.querySelector("#root");
+let body = document.body;
+let menu = document.querySelector(".content1");
+let exitBtn = document.querySelector(".exit");
+exitBtn.addEventListener("click", (e) => {
+  loaderBackground.style.display = "none";
+  menu.style.animation =
+    "hideElement 0.2s linear 1 forwards, moveBackground1 15s linear infinite";
+  menu.style.animationFillMode = "forwards";
+  setTimeout(() => {
+    menu.style.display = "none";
+  }, 200);
+});
+
+function showLoader() {
+  loaderBackground.style.display = "flex";
+}
+function showContent(title, message) {
+  titleContent.innerHTML = title;
+  returnedContent.innerHTML = message;
+  menu.style.animation =
+    "showElement 0.2s linear 1 reverse, moveBackground1 15s linear infinite";
+  menu.style.animationFillMode = "forwards";
+  setTimeout(() => {
+    menu.style.display = "block";
+  }, 200);
+}
